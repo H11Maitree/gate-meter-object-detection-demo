5d83cd23182f92137bd04ecd7f9114ae71bbd3cf
Matthew <h.mattjunk@gmail.com>
2024-01-07 03:05:49 +0800

    chore: change version of model

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ function enableCam(event) {
 var model = undefined;
 
 tflite.ObjectDetector.create(
-  "gate-classification.tflite"
+  "gate-classification-V2.tflite"
 ).then((loadedModel) => {
   model = loadedModel;
   // Show demo section now model is ready to use.
